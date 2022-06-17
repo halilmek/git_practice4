d6fe94cecf2b3352e580a762da52fe850b9f3fe7
halilozturk <ztrkhll58@gmail.com>
2022-06-17 18:11:02 +0200

    1st commit

---
+++ src/hello/TestCase003.java
@@ -0,0 +1,10 @@
+package hello;
+
+public class TestCase003 {
+
+    public static void main(String[] args) {
+
+        System.out.println("1st commit");
+        System.out.println("\"-------   \" = " + "-------   ");
+    }
+}
