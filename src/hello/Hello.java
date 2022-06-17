98f5836b20395b2a31210121b849197c04e103f9
halilozturk <ztrkhll58@gmail.com>
2022-06-17 13:36:33 +0200

    printing hello world

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,15 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("\"hello word\" = " + "hello word");
+        System.out.println("\"hello world\" = " + "hello world");
+
+        System.out.println("\"------------------\" = " + "------------------");
+        System.out.println("\"hello world\" = " + "hello world");
+        System.out.println("\"-------------\" = " + "-------------");
+        System.out.println("\"hello world\" = " + "hello world");
+        System.out.println("\"-------------\" = " + "-------------");
+        System.out.println("eu8 is learning git");
+
+
     }
 }
