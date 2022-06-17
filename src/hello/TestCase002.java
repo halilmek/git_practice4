f2ca316c06c6d38eda40a62ac87a30b26c853234
halilozturk <ztrkhll58@gmail.com>
2022-06-17 13:55:22 +0200

    TestCase002 Ended

--- src/hello/TestCase002.java
+++ src/hello/TestCase002.java
@@ -4,5 +4,8 @@ public class TestCase002 {
 
     public static void main(String[] args) {
         System.out.println("test case 002 was created");
+        System.out.println("\"------------         \" = " + "------------         ");
+        System.out.println("TestCase002 is ended");
+        System.out.println("\"----------        \" = " + "----------        ");
     }
 }
