d0933de7f47927d349782f626c1d4f1b7a464fee
halilozturk <ztrkhll58@gmail.com>
2022-06-17 13:44:33 +0200

    step 2

--- src/hello/TestCase001.java
+++ src/hello/TestCase001.java
@@ -6,5 +6,7 @@ public class TestCase001 {
 
         System.out.println("\"testcase 001 was started\" = " + "testcase 001 was started");
         System.out.println("step1");
+        System.out.println("step2");
+        System.out.println("step3");
     }
 }
