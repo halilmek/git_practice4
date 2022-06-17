51eb832f49ecc8f43e3eb6b5d81f404d351f088a
halilozturk <ztrkhll58@gmail.com>
2022-06-17 13:42:51 +0200

    testcase 001 was started

---
+++ src/hello/TestCase001.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class TestCase001 {
+    public static void main(String[] args) {
+
+
+        System.out.println("\"testcase 001 was started\" = " + "testcase 001 was started");
+    }
+}
