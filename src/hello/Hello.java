23d85952c0e085f6493019f6a2d08393f6b2f1d7
halilozturk <ztrkhll58@gmail.com>
2022-06-17 13:37:25 +0200

    printing who learns git

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -11,6 +11,7 @@ public class Hello {
         System.out.println("\"-------------\" = " + "-------------");
         System.out.println("\"hello world\" = " + "hello world");
         System.out.println("\"-------------\" = " + "-------------");
+        System.out.println("\"-------------\" = " + "-------------");
         System.out.println("eu8 is learning git");
 
 
