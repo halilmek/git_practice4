a24ea8ac224d0009a5cfe2cf5ca6fe0fd3ee0ba5
halilozturk <ztrkhll58@gmail.com>
2022-06-18 12:07:46 +0200

    ready to review

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -6,5 +6,7 @@ public class TestCase004 {
     public static void main(String[] args) {
 
         System.out.println("cool code here");
+        System.out.println("\"--------      \" = " + "--------      ");
+        System.out.println("completed, ready to review");
     }
 }
